24a7d038209adebed8bae18da232de0dcc0c396d
dthgs <daniel.thor.gunnlaugsson@gmail.com>
2014-10-19 19:00:36 +0000

    Refactoring the add function

--- src/main/java/is/ru/stringcalculator/Calculator.java
+++ src/main/java/is/ru/stringcalculator/Calculator.java
@@ -1,14 +1,14 @@
 package is.ru.stringcalculator;
 
 public class Calculator {
-
+	
 	public static int add(String text){
-		if(text.equals("")){
+		if(text.equals(""))
 			return 0;
-		}
-		else if((text.contains(",") || text.contains("\n")) || text.startsWith("//")){
+		if(text.contains("\n"))
+			text = text.replace("\n", ",");
+		if(text.contains(","))
 			return sum(splitNumbers(text));
-		}
 		else
 			return 1;
 	}
@@ -18,9 +18,9 @@ public class Calculator {
 	}
 
 	private static String[] splitNumbers(String numbers){
-	    return numbers.split("[^0-9-]+");
+		return numbers.split(",");
 	}
-      
+	
     private static int sum(String[] numbers){
  	    int total = 0;
         for(String number : numbers){
